a1fbeb881cbedaad0ece25183403a8518394b792
coscristian <crisquesadaco@gmail.com>
2022-12-04 12:36:36 -0500

    Added CRUD operations to BookController

--- controller/book.py
+++ controller/book.py
@@ -1,15 +1,42 @@
 from controller.connection import connect
+from model.book import BookModel
 
 class BookController:
     def list_books():
         sql = "SELECT * FROM Book"
+        
         db = connect()
         cursor = db.cursor()
         cursor.execute(sql)
-        result = cursor.fetchall() #Get all the rows of the previous querie
-        
-        
-            
-        
+        results = cursor.fetchall() #Get all the rows of the previous querie
         db.close()
         
+        books = list(map(lambda t: BookModel(t[0], t[1], t[2], t[3], t[4], []), results))
+        return books  
+    
+    def insert(self, book: BookModel):
+        sql = "INSERT INTO Book (title, amount, amount_available, topic) VALUES ('%s', %d, %d, %s)" % (book.get_title(), book.get_amount(), book.get_amount_available(), book.get_topic())
+        
+        db = connect()
+        cursor = db.cursor()
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+        
+    def update(self, book: BookModel):
+        sql = "UPDATE Book SET title = '%s', amount = %d, amount_available = %d, topic = '%s' WHERE id = %d" % (book.get_title(), book.get_amount(), book.get_amount_available(), book.get_topic(), book.get_id())
+        
+        db = connect()
+        cursor = db.cursor()
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+        
+    def delete(self, book_id: int):
+        sql = "DELETE FROM Book WHERE id = %d" % (book_id)
+        
+        db = connect()
+        cursor = db.cursor()
+        cursor.execute(sql)
+        db.commit()
+        db.close()
